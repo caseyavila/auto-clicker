70f7e55de52697c0fa54019cea653a7183f8e160
Casey Avila <53847249+kc-kc@users.noreply.github.com>
2019-08-27 14:18:55 -0700

    Allow script to activate upon profile activation

--- autoclicker.lua
+++ autoclicker.lua
@@ -5,6 +5,13 @@ math.randomseed(42069)
 math.random(); math.random(); math.random()
 
 function OnEvent(event, arg)
+	--OutputLogMessage("event=%s, arg=%s\n", event, arg)
+	if(event == "PROFILE_ACTIVATED") then
+		--profile has been activated
+	end
+	if(event == "PROFILE_DEACTIVATED") then
+		--profile has been deactivated
+	end
 	while IsMouseButtonPressed(5) do
 		PressAndReleaseMouseButton(1)
 		Sleep(math.random(minDelay, maxDelay))
